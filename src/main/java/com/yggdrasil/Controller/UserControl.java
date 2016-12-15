b2f1dd7cb2c35beb2d63e1c80b9773d37e24a96e
Yggdrasilil <517334437@qq.com>
2016-12-15 17:36:59 +0800

    注册以及用户信息修改

--- src/main/java/com/yggdrasil/Controller/UserControl.java
+++ src/main/java/com/yggdrasil/Controller/UserControl.java
@@ -2,6 +2,7 @@ package com.yggdrasil.Controller;
 
 
 import com.yggdrasil.Repository.UserRepository;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,5 +30,22 @@ public class UserControl {
      @RequestMapping(value = "/getName",method = RequestMethod.GET)
      public String getPostUser(String id) {
          return userRepository.findById(id).getName();
+     }
+     @RequestMapping("/getCurr")
+     public User getUser(HttpServletRequest httpServletRequest){
+         return (User)httpServletRequest.getSession().getAttribute("user");
+     }
+
+     @RequestMapping(value = "/modifyUserInfo", method = RequestMethod.POST)
+     public String modifyUserInfo(@RequestBody User user_info, HttpServletRequest httpServletRequest) {
+         User user = (User)httpServletRequest.getSession().getAttribute("user");
+         user_info.setId(user.getId());
+         user_info.setGroup_id(user.getGroup_id());
+         user_info.setAuthority_id(user.getAuthority_id());
+         user_info.setSex(user.getSex());
+         user_info.setAcc_point(user.getAcc_point());
+         userRepository.save(user_info);
+         httpServletRequest.getSession().setAttribute("user",user_info);
+         return "success";
      }
 }
