b83287e5ce6c3baf885633e2c20a07c590fa22aa
Yggdrasilqh <yggdrasilqh@gmail.com>
2016-12-19 14:37:10 +0800

    用户经验查看及部分用户管理功能

--- src/main/java/com/yggdrasil/Repository/ModeratorRepository.java
+++ src/main/java/com/yggdrasil/Repository/ModeratorRepository.java
@@ -4,6 +4,7 @@ import com.yggdrasil.Entity.Moderator;
 import com.yggdrasil.Entity.ModeratorPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,4 +14,7 @@ import java.util.List;
 public interface ModeratorRepository extends JpaRepository<Moderator,ModeratorPK> {
     @Query(value = "select * from moderator where user_id = :user_id and layout_id = :layout_id",nativeQuery = true)
     public List<Moderator> findByUser_idAndLayout_id(String user_id,int layout_id);
+
+    @Query(value = "select layout_id from moderator where user_id = :user_id", nativeQuery = true)
+    public List<Integer> findByUser_id(@Param("user_id") String user_id);
 }
